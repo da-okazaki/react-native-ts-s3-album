880fb62fbe535ddc3d4466b2788fa6fa350164eb
da-okazaki <da-okazaki@outlook.com>
2021-08-03 17:16:14 +0900

    [add] Styleファイル追加

---
+++ styles/global-style.ts
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    // width: "100vh",
+    // height: "100vw",
+  },
+
+  main: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  photoStyle: {
+    width: "100%",
+    height: 300,
+    // objectFit: "fill",
+    borderRadius: 10,
+    // height: "100%",
+  },
+});
