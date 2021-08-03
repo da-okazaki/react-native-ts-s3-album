3649695793c1d16015388200b52cc6606d105fe2
da-okazaki <da-okazaki@outlook.com>
2021-08-03 17:14:45 +0900

    [add] react-native-paper 追加

--- App.tsx
+++ App.tsx
@@ -1,21 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/* react */
+import * as React from "react";
+import { View } from "react-native";
+
+/* expo */
+import { registerRootComponent } from "expo";
+import { StatusBar } from "expo-status-bar";
+
+/* react native paper */
+import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
+
+/* components */
+import { Topbar, S3Gallery } from "./components";
+
+/* styles */
+import { styles } from "./styles/global-style";
+
+/* AWS Amplify */
+import Amplify from "aws-amplify";
+import awsconfig from "./src/aws-exports";
+Amplify.configure(awsconfig);
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "tomato",
+    accent: "yellow",
+  },
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <View style={styles.container}>
+        <Topbar />
+        <S3Gallery />
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
