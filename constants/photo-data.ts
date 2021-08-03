aa398e0bb2946779b1dfcf089adcec3bebc91e40
da-okazaki <da-okazaki@outlook.com>
2021-08-03 17:16:37 +0900

    [add] 画像データ追加

---
+++ constants/photo-data.ts
@@ -0,0 +1,34 @@
+import { Photos } from "../shared/types";
+
+export const photos: Photos[] = [
+  {
+    src: "https://i.pinimg.com/474x/d2/14/db/d214db6421abcdafc0be9ef64feab1bc.jpg",
+    width: 4,
+    height: 3,
+  },
+  {
+    src: "https://i.pinimg.com/474x/87/d2/94/87d294f0c7210f9162cb17d686c4ddf5.jpg",
+    width: 1,
+    height: 1,
+  },
+  {
+    src: "https://i.pinimg.com/474x/97/7e/6c/977e6cbaf0734248fbb09931002cf7c6.jpg",
+    width: 1,
+    height: 1,
+  },
+  {
+    src: "https://i.pinimg.com/474x/66/0d/4c/660d4ce6cd13e65a042087bd025a3c59.jpg",
+    width: 1,
+    height: 1,
+  },
+  {
+    src: "https://i.pinimg.com/474x/56/f3/46/56f346a93f14c5e56c0326a5da865193.jpg",
+    width: 1,
+    height: 1,
+  },
+  {
+    src: "https://i.pinimg.com/474x/fc/f1/e4/fcf1e436486fd5a885fb141316bf4ec7.jpg",
+    width: 1,
+    height: 1,
+  },
+];
